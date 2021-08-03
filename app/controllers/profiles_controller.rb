a24e565080e9fb0b165d3a41fc3676037cf54594
hikaru-webcamp <skyvader0524@gmail.com>
2021-08-03 17:27:49 +0900

    N+１問題解消練習

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -4,19 +4,20 @@ class ProfilesController < ApplicationController
     raise Forbidden unless user_safe?
 
     @skill_categories = user_reccomend_skill_categories
-    @articles = @user.articles
-  end
+    @articles = @user.articles.preload(:tags)
+    #ループ内で関連テーブルの値を使用する場合 pre
+    end
 
   private
-
+#ユーザーに紐づく全てのユーザー凍結情報の終了時間と現在時間を比較し、現在凍結期間外である事の確認
   def user_safe?
-    @user.user_cautions.all? do |user_caution|
-      Time.zone.now > user_caution.caution_freeze.end_time
-    end
+    @user.user_cautions.joins(:caution_freeze).
+    where("caution_freezes.end_time > ?", Time.zone.now).blank?
   end
 
   def user_reccomend_skill_categories
-    @user.skills.map(&:skill_category).
-      filter { |skill_category| skill_category.reccomend }.uniq
+    SkillCategory.eager_load(:skills).
+    where(reccomend: true).
+    where(skills: { user_id: @user.id })
   end
 end
